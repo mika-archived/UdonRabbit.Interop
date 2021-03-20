5fad3d8db42fd55a7c6c0281b5242695bbcc8591
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-21 01:29:55 +0900

    feat: add toggle button

---
+++ Assets/Mochizuki/VRChat/Interop/Programs/EventListener.cs
@@ -0,0 +1,105 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using UdonSharp;
+
+namespace Mochizuki.VRChat.Interop
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class EventListener : UdonSharpBehaviour
+    {
+        private object _object;
+
+        public void SetArgument(object obj)
+        {
+            _object = obj;
+        }
+
+        public object GetArgument()
+        {
+            return _object;
+        }
+
+        #region OnInteracted
+
+        private bool _isInteracted;
+
+        public void OnInteracted()
+        {
+            _isInteracted = true;
+        }
+
+        public bool IsInteracted()
+        {
+            if (!_isInteracted)
+                return false;
+
+            _isInteracted = false;
+            return true;
+        }
+
+        #endregion
+
+        #region OnDropped
+
+        private bool _isDropped;
+
+        public void OnDropped()
+        {
+            _isDropped = true;
+        }
+
+        public bool IsDropped()
+        {
+            if (!_isDropped)
+                return false;
+
+            _isDropped = false;
+            return true;
+        }
+
+        #endregion
+
+        #region OnPickupped
+
+        private bool _isPickupped;
+
+        public void OnPickupped()
+        {
+            _isPickupped = true;
+        }
+
+        public bool IsPickupped()
+        {
+            if (!_isPickupped)
+                return false;
+
+            _isPickupped = false;
+            return true;
+        }
+
+        #endregion
+
+        #region OnPickupUseDowned
+
+        private bool _isPickupUseDowned;
+
+        public void OnPickupUseDowned()
+        {
+            _isPickupUseDowned = true;
+        }
+
+        public bool IsPickupUseDowned()
+        {
+            if (!_isPickupUseDowned)
+                return false;
+
+            _isPickupUseDowned = false;
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Mochizuki/VRChat/Interop/Programs/ToggleButton.cs
@@ -0,0 +1,99 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using UdonSharp;
+
+using UnityEngine;
+
+using VRC.SDKBase;
+
+namespace Mochizuki.VRChat.Interop
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class ToggleButton : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private bool initialState;
+
+        [SerializeField]
+        private EventListener listener;
+
+        [SerializeField]
+        private InteropLogger logger;
+
+        private bool _hasListener;
+        private bool _hasLogger;
+
+        [UdonSynced]
+        private bool _state;
+
+        private void Start()
+        {
+            _state = initialState;
+            _hasListener = listener != null;
+            _hasLogger = logger != null;
+        }
+
+        public override void Interact()
+        {
+            if (!_hasListener)
+                return;
+
+            if (!Networking.IsOwner(Networking.LocalPlayer, gameObject))
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+            if (_hasLogger)
+                logger.LogInfo($"{nameof(Interact)} - Request Manual Synchronization");
+
+            _state = !_state;
+
+            // for owner
+            if (_hasListener)
+            {
+                listener.SetArgument(_state);
+                listener.OnInteracted();
+            }
+
+            RequestSerialization(); // for other users
+        }
+
+        public override bool OnOwnershipRequest(VRCPlayerApi requestingPlayer, VRCPlayerApi requestedOwner)
+        {
+            if (_hasLogger)
+                logger.LogInfo($"{nameof(OnOwnershipRequest)} - Request Transfer Ownership to {requestedOwner.displayName} - Approved");
+            return true;
+        }
+
+        public override void OnOwnershipTransferred(VRCPlayerApi player)
+        {
+            if (_hasLogger)
+                logger.LogInfo($"{nameof(OnOwnershipTransferred)} - Ownership is transferred to {player.displayName}");
+        }
+
+        public override void OnPreSerialization()
+        {
+            if (_hasLogger)
+            {
+                logger.LogInfo($"{nameof(OnPreSerialization)} - Request Serialization Variables for Synchronization");
+                logger.LogInfo($"{nameof(OnPreSerialization)} - Current State is {!_state}");
+            }
+        }
+
+        public override void OnDeserialization()
+        {
+            if (_hasLogger)
+            {
+                logger.LogInfo($"{nameof(OnDeserialization)} - Received Serialization Variables for Synchronization");
+                logger.LogInfo($"{nameof(OnDeserialization)} - Current State is {_state}");
+            }
+
+            if (_hasListener)
+            {
+                listener.SetArgument(_state);
+                listener.OnInteracted();
+            }
+        }
+    }
+}
