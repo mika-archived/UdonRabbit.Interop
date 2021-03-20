32502cad1de47c4f15047e6f9b6397c793d3037c
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-21 01:26:18 +0900

    feat: add debug console

---
+++ Assets/Mochizuki/VRChat/Interop/Programs/DebugConsole.cs
@@ -0,0 +1,62 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using TMPro;
+
+using UdonSharp;
+
+using UnityEngine;
+
+namespace Mochizuki.VRChat.Interop
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class DebugConsole : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI console;
+
+        [SerializeField]
+        private int maxLines;
+
+        private bool _hasConsole;
+        private int _len;
+        private string[] _lines;
+        private int _ptr;
+
+        private void Start()
+        {
+            _hasConsole = console != null;
+            _lines = new string[maxLines];
+        }
+
+        public void AddLine(string line)
+        {
+            _lines[_ptr++] = $"{line}\n";
+            _len++;
+
+            if (_ptr >= maxLines)
+                _ptr = 0;
+            if (_len >= maxLines)
+                _len = maxLines;
+
+            var text = "";
+            for (var i = 0; i < _len; i++)
+                text += _lines[(maxLines + _ptr - _len + i) % maxLines];
+
+            console.text = text;
+        }
+
+        public void Clear()
+        {
+            _len = 0;
+            _ptr = 0;
+
+            for (var i = 0; i < maxLines; i++)
+                _lines[i] = "";
+
+            console.text = "";
+        }
+    }
+}
