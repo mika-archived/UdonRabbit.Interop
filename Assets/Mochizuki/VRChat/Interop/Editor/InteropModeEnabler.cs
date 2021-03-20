0eb34a5d39f5c7202d481cc7c2df0d832bad677a
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-21 01:20:11 +0900

    feat: add base editor script

---
+++ Assets/Mochizuki/VRChat/Interop/Editor/InteropModeEnabler.cs
@@ -0,0 +1,28 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using System.Linq;
+
+using UnityEditor;
+
+namespace Mochizuki.VRChat.Interop
+{
+    [InitializeOnLoad]
+    public static class InteropModeEnabler
+    {
+        private const string Preprocessor = "MOCHIZUKI_INTEROP";
+
+        static InteropModeEnabler()
+        {
+            var targetGroup = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup).Split(';').ToList();
+            if (symbols.Contains(Preprocessor))
+                return;
+
+            symbols.Add(Preprocessor);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, string.Join(";", symbols));
+        }
+    }
+}
