93885549e94be4c14185f1c87eb9a9d2d5c9a232
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-21 01:27:10 +0900

    feat: add validator attribute for debug usage

---
+++ Assets/Mochizuki/VRChat/Interop/Validator/Attributes/RequestArgumentTypeAttribute.cs
@@ -0,0 +1,22 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using System;
+using System.Diagnostics;
+
+namespace Mochizuki.VRChat.Interop.Validator.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    [Conditional("UNITY_EDITOR")]
+    public class RequestArgumentTypeAttribute : Attribute
+    {
+        public Type RequestedType { get; }
+
+        public RequestArgumentTypeAttribute(Type t)
+        {
+            RequestedType = t;
+        }
+    }
+}
