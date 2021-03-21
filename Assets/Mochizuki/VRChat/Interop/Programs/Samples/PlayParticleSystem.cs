2cb06938662793be17981597a5d7b196cee4dc9d
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-21 19:08:47 +0900

    feat: add stateless button

---
+++ Assets/Mochizuki/VRChat/Interop/Programs/Samples/PlayParticleSystem.cs
@@ -0,0 +1,38 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using UdonSharp;
+
+using UnityEngine;
+
+namespace Mochizuki.VRChat.Interop.Samples
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class PlayParticleSystem : UdonSharpBehaviour
+    {
+        private void Start()
+        {
+            particle.Stop(true);
+        }
+
+        private void Update()
+        {
+            if (!listener.IsInteracted())
+                return;
+
+            particle.Play(true);
+        }
+
+#pragma warning disable CS0649
+
+        [SerializeField]
+        private EventListener listener;
+
+        [SerializeField]
+        private ParticleSystem particle;
+
+#pragma warning restore CS0649
+    }
+}
---
+++ Assets/Mochizuki/VRChat/Interop/Programs/StatelessButton.cs
@@ -0,0 +1,53 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using UdonSharp;
+
+using UnityEngine;
+
+using VRC.Udon.Common.Interfaces;
+
+namespace Mochizuki.VRChat.Interop
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class StatelessButton : UdonSharpBehaviour
+    {
+        private bool _hasListener;
+
+        private bool _hasLogger;
+
+        private void Start()
+        {
+            _hasListener = listener != null;
+            _hasLogger = logger != null;
+        }
+
+        public override void Interact()
+        {
+            if (_hasLogger)
+                logger.LogInfo($"{nameof(Interact)} - {nameof(SendCustomNetworkEvent)}({nameof(OnInteracted)})");
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(OnInteracted));
+        }
+
+        public void OnInteracted()
+        {
+            if (_hasLogger)
+                logger.LogInfo($"{nameof(OnInteracted)} - Event Called");
+
+            if (_hasListener)
+                listener.OnInteracted();
+        }
+
+#pragma warning disable CS0649
+
+        [SerializeField]
+        private EventListener listener;
+
+        [SerializeField]
+        private InteropLogger logger;
+
+#pragma warning restore CS0649
+    }
+}
